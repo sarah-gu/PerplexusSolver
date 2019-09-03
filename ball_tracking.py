681eff652ad6db9ec48e59773b82cdecc007330b
Sarah Gu <guwen888@gmail.com>
2019-09-03 11:54:10 -0400

    hsv values

--- ball_tracking.py
+++ ball_tracking.py
@@ -18,8 +18,10 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenLower = (34,41,136) #silver ball hsv
+greenUpper= (116,255,255)
+#greenLower = (0,106,226)
+#greenUpper= (179,255,255)#orange ball hsv
 pts = deque(maxlen=args["buffer"])
  
 # if a video path was not supplied, grab the reference
