abab2ab0227ec2476b034e818679d7bbd836e37f
Sarah Gu <guwen888@gmail.com>
2020-03-05 11:27:20 -0500

    darknet updates

--- Sarah/Software/runYOLO/run.sh
+++ Sarah/Software/runYOLO/run.sh
@@ -1,1 +1,1 @@
-python3 yolo.py -w ../darknet/backup/yolov3-tiny.backup -cfg ../darknet/custom/yolov3-tiny.cfg -m ../darknet/cfg/ -l ../darknet/custom/objects.names -c 0 -v ../PerplexusVid.mp4
+python3 yolo.py -w ../darknet53/backup/yolov3-tiny_60000.weights -cfg ../darknet53/custom/yolov3-tiny.cfg -m ../darknet53/cfg/ -l ../darknet53/custom/objects.names -c 0 -v ../PerplexusVid.mp4
